786b4d1b6178e89da5937c9f41c5a25dd7ee1fc4
MrQS94 <quezale@outlook.com>
2023-03-19 14:36:06 -0600

    1.0

--- src/controlador/ControladorSimulacion.java
+++ src/controlador/ControladorSimulacion.java
@@ -4,9 +4,6 @@
  */
 package controlador;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
